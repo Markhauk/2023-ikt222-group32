0681404d6a86bd3667631f8895bde660c7c41819
Markus Haukas <43339372+Markhauk@users.noreply.github.com>
2023-12-11 19:25:39 +0100

    Added code for opening chrome  that navigates to loginpage
    Used selenium with python

--- code/solutions/attack_1/main.py
+++ code/solutions/attack_1/main.py
@@ -1,18 +1,53 @@
-total_time = 0
+import string
+#https://stackoverflow.com/questions/13166395/fill-input-of-type-text-and-press-submit-using-python
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-a = 'Hallakellær'
-b = 'tullingær'
+webpage = "https://portal.regjeringen.uiaikt.no/"  # Edit the URL
+searchterm = "YourUsername"  # Edit the username
 
-if len(a) != len(b):
-    print(total_time)
-    exit()
+# Ensure you have the correct path to your ChromeDriver executable
+chrome_driver_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 
-for i in range(len(a)):
-    if a[i] != b[i]:
-        print(total_time)
-        exit()
-    total_time += 1
+# Set up the Chrome WebDriver
+driver = webdriver.Chrome()
+driver.get(webpage)
 
-print(total_time)
+# Wait for user input before closing the browser
+input("Press Enter to close the browser...")
+
+# Close the browser window
+driver.quit()
+
+# Locate the search box and submit button by class name
+#sbox = driver.find_element_by_class_name("formcontroll")
+#submit = driver.find_element_by_class_name("btn btn-primary btn-block")
+
+# Input the search term and submit the form
+#sbox.send_keys(searchterm)
+#submit.click()
+
+# Close the WebDriver after use
+#driver.quit()
+
+
+
+username = "jonas.dahl"
+
+passwordguess = ""
+
+passwordlenght = 17
+
+allcharacters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+
+
+
+
+
+print(username)
+
+
+
+
 
 
